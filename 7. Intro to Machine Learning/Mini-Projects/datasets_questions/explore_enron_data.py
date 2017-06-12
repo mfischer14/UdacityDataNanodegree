a833374b5d1203c5a98472b3442c2cfc1dcf4d0e
Markus G. Fischer <mgfischer@gmail.com>
2017-06-12 00:23:01 -0400

    First changes to ENRON project

--- 7. Intro to Machine Learning/Mini-Projects/datasets_questions/explore_enron_data.py
+++ 7. Intro to Machine Learning/Mini-Projects/datasets_questions/explore_enron_data.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-""" 
+"""
     Starter code for exploring the Enron dataset (emails + finances);
     loads up the dataset (pickled dict of dicts).
 
@@ -12,11 +12,17 @@
     but here's an example to get you started:
 
     enron_data["SKILLING JEFFREY K"]["bonus"] = 5600000
-    
+
 """
 
 import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
+print("Number of people: {}".format(len(enron_data)))
+print("Number of features: {}".format(len(enron_data["METTS MARK"])))
 
+# Access data with enron_data["LASTNAME FIRSTNAME MIDDLEINITIAL"]["feature_name"]
 
+print("Total value of the stock of James Prentice: {}".format(enron_data["PRENTICE JAMES"]["total_stock_value"]))
+print("Number of emails from Wesley Colwell: {}".format(enron_data["COLWELL WESLEY"]["from_this_person_to_poi"]))
+print("Number of stock options exercised by Jeffrey K Skilling: {}".format(enron_data["SKILLING JEFFREY K"]["exercised_stock_options"]))
